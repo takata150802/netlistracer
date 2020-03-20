36655bc60bf181a8ef7bf87df0b6de379414fcbe
RyoTakata <takata@hal.ipc.i.u-tokyo.ac.jp>
2020-03-21 02:30:15 +0900

    [refactor]

--- show_module_tree.py
+++ show_module_tree.py
@@ -16,6 +16,10 @@ from pyverilog.vparser.ast import Instance
 from pyverilog.vparser.ast import Input
 from pyverilog.vparser.ast import Output
 from pyverilog.vparser.ast import PortArg
+def debug(x):
+    print(x, file=sys.stderr, end=' ')
+    return
+
 def module_tree(self, buf=sys.stdout, offset=0, showlineno=True):
     indent = 2
     lead = ' ' * offset
@@ -41,6 +45,7 @@ def module_tree(self, buf=sys.stdout, offset=0, showlineno=True):
 
 
 def get_node(self, fn, buf=sys.stderr, offset=0, showlineno=True, ret=[]):
+
     indent = 2
     lead = ' ' * offset
     
@@ -54,7 +59,7 @@ def get_node(self, fn, buf=sys.stderr, offset=0, showlineno=True, ret=[]):
         if showlineno:
             buf.write(' (at %s)' % self.lineno)
         buf.write('\n')
-        return ret
+        return
 
     for c in self.children():
         c.get_node(fn, buf, offset + indent, showlineno, ret)
@@ -72,28 +77,51 @@ digraph {
 gen_dot_footer = "}"
 Instance.id_ = 0
 def gen_dot(self, ls_module):
-    if isinstance(self, Instance):
+    debug("gen_dot:")
+    debug(self)
+    debug('\n')
+    debug("ls_module:")
+    debug(ls_module)
+    debug('\n')
+
+    if isinstance(self, ModuleDef):
+        for i in self.ls_instance:
+            i.gen_dot(ls_module)
+        return
+
+    elif isinstance(self, Instance):
+        """
+        TODO: visitorパターンにrefactorする
+              ↓ 2つを分離する点がvisitor patternを知ってる人にはわかりやすいかも
+              - selfに対するdot生成処理
+              - ls_instanceやportを呼び出す部分
+        """
         print ('subgraph cluster%d {'%Instance.id_)
         print ("  graph [label = \"%s:%s\"];"%(self.module, self.name))
         print ("tmp%d;"%Instance.id_)
+
         Instance.id_ += 1
-        for m in ls_module:
-            ll = m.get_node(lambda x: isinstance(x, ModuleDef) and x.name == self.module, ret=[])
-            print(ls_module, ll, len(ll), file=sys.stderr)
-            if len(ll) > 1:
-                assert (False), "Fuck"
-            elif len(ll) == 1:
-                ll[0].gen_dot(ls_module)
-            else :
-                pass
+        ll = [i for i in ls_module if isinstance(i, ModuleDef) and i.name == self.module]
+        debug("ll = [i for i in ls_module if isinstance(i, ModuleDef) and i.name == self.module]:")
+        debug(ll)
+        debug(self.module)
+        debug('\n')
+        emsg = "\n" \
+             + "multiple declear of module `" + str(self.module) + "` is detected.\n" \
+             + "but this check is NOT enough 'cause of TOP module multiple declear.\n"
+        assert (len(ll) == 1 or len(ll) == 0), emsg
+        if len(ll) == 1:
+            ll[0].gen_dot(ls_module)
+        else :
+            pass
         print ('}')
-        for c in self.children():
-            c.gen_dot(ls_module)
         return
+
     else:
         for c in self.children():
             c.gen_dot(ls_module)
-    return
+        return
+    assert (False), "Unreachable."
 
 def main():
     INFO = "Verilog code parser"
@@ -130,13 +158,18 @@ def main():
     ast, directives = parse(filelist,
                             preprocess_include=options.include,
                             preprocess_define=options.define)
-    ls_module = ast.get_node(lambda x: isinstance(x, ModuleDef), ret=[])
+    ls_module = []
+    ast.get_node(lambda x: isinstance(x, ModuleDef), ret=ls_module)
     for m in ls_module:
-        m.ls_input  = m.get_node(lambda x: isinstance(x, Input), ret=[])
-        m.ls_output  = m.get_node(lambda x: isinstance(x, Output), ret=[])
-        m.ls_instance  = m.get_node(lambda x: isinstance(x, Instance), ret=[])
+        m.ls_input = []
+        m.ls_output = []
+        m.ls_instance = []
+        m.get_node(lambda x: isinstance(x, Input), ret=m.ls_input)
+        m.get_node(lambda x: isinstance(x, Output), ret=m.ls_output)
+        m.get_node(lambda x: isinstance(x, Instance), ret=m.ls_instance)
         for i in m.ls_instance:
-            i.ls_port  = m.get_node(lambda x: isinstance(x, PortArg), ret=[])
+            i.ls_port = []
+            i.get_node(lambda x: isinstance(x, PortArg), ret=i.ls_port)
     print (gen_dot_header)
     ls_module[0].gen_dot(ls_module)
     print (gen_dot_footer)
